609ac5c5c12e7392fc37e5021f3dd440220deb78
brent-bouslog-comed <40209740+brent-bouslog-comed@users.noreply.github.com>
2018-06-26 13:45:53 -0500

    Add files via upload

--- NumberLED/NumberLED.ino
+++ NumberLED/NumberLED.ino
@@ -13,175 +13,281 @@
 
 // How many leds are in the strip?
 #define NUM_LEDS 70
+#define MAX_LEDS 72
+#define NUM_CHAR 3
 
 // Data pin that led data will be written out over
-#define DATA_PIN 3
+#define TENS_PIN 6
+#define ONES_PIN 7
+#define DEC_PIN 8
+
+#define TENS_POS 0
+#define ONES_POS 1
+#define DEC_POS 2
 
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 //#define CLOCK_PIN 8
 
 // This is an array of leds.  One item for each led in your strip.
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_CHAR][MAX_LEDS];
+double prevNum[3] = {-1,-1,-1};
 
-int num = 0;
+double num = 0.0;
+int count = 0;
 
 // This function sets up the ledsand tells the controller about them
 void setup() {
-  // sanity check delay - allows reprogramming if accidently blowing power w/leds
-    delay(2000);
+     // Serial.begin(9600);
+  
+      FastLED.addLeds<WS2811, TENS_PIN, GRB>(leds[TENS_POS], MAX_LEDS);
+      FastLED.addLeds<WS2811, ONES_PIN, GRB>(leds[ONES_POS], MAX_LEDS);
+      FastLED.addLeds<WS2811, DEC_PIN, GRB>(leds[DEC_POS], MAX_LEDS);
 
-      // Uncomment one of the following lines for your leds arrangement.
-      // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-      FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
-      // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA104, DATA_PIN>(leds, NUM_LEDS);
-      // FastLED.addLeds<WS2811_400, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+      clearNumber(TENS_POS);
+      clearNumber(ONES_POS);
+      clearNumber(DEC_POS);
 
-      // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
-      
-      // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      setDecPoint();
+      Serial.print("Dec Done");
 }
 
 // This function runs over and over, and is where you do the magic to light
 // your leds.
 void loop() {
-   // Move a single white led 
-   setNumber(num);
-   num = (num + 1) % 10;
+   count++;
+   if(count % 1 == 0)
+   {
+      num += .1;// * ((count % 4) - 1);
+      //fgnum += (random(0,3)/10.0)*(random(0,3) - 1);
+      if(num < 0)
+        num == 0;
+   }
+   if(count % 20 == 0)
+   {
+      for(int i = 0; i < 3; i++)
+      {
+        prevNum[i] = -1;
+        clearNumber(i);
+      }
+      
+      delay(1000);
+   }
+
+   //Serial.println(String(num, 1));
+   writeNum2(num);
+   
    delay(500);
 }
 
-void setNumber(int num){
+void writeNum2(double value)
+{
+    int val = (int)(value * 10);
+
+    double num[3];
+    num[DEC_POS] = val % 10;
+    val = (val - (val % 10))/10;
+    num[ONES_POS] = val % 10;
+    val = (val - (val % 10))/10;
+    num[TENS_POS] = val % 10;
+
+    for(int i = 0; i < 3; i++)
+    {
+      if(num[i] != prevNum[i])
+      {
+        setNumber(num[i], i);
+        prevNum[i] = num[i];
+      }
+    }
+}
+
+void writeNum(double value)
+{
+  int val = (int)(value * 10);
+
+  if(val > 0 && val < 10)
+  {
+    clearNumber(TENS_POS);
+    clearNumber(ONES_POS);
+
+    if(val % 10 != prevNum[DEC_POS])
+    {
+      setNumber((val % 10), DEC_POS);
+      prevNum[DEC_POS] = val % 10;
+    }
+  }
+  else if(val >= 10 && val < 100)
+  {
+    clearNumber(TENS_POS);
+
+    if(val % 10 != prevNum[DEC_POS])
+    {
+      setNumber((val % 10), DEC_POS);
+      prevNum[DEC_POS] = val % 10;
+    }
+    
+    val = (val - (val % 10))/10;
+
+    if(val % 10 != prevNum[ONES_POS])
+    {
+      setNumber((val % 10), ONES_POS);
+      prevNum[ONES_POS] = val % 10;
+    }
+    Serial.print(val);
+  }
+  else if(val >= 100)
+  {
+    if(val % 10 != prevNum[DEC_POS])
+    {
+      setNumber((val % 10), DEC_POS);
+      prevNum[DEC_POS] = val % 10;
+    }
+    
+    val = (val - (val % 10))/10;
+    
+    if(val % 10 != prevNum[ONES_POS])
+    {
+      setNumber((val % 10), ONES_POS);
+      prevNum[ONES_POS] = val % 10;
+    }
+    
+    val = (val - (val % 10))/10;
+    
+    if(val % 10 != prevNum[TENS_POS])
+    {
+      setNumber((val % 10), TENS_POS);
+      prevNum[TENS_POS] = val % 10;
+    }
+  }
+}
+
+void setDecPoint(void)
+{
+   for(int i = NUM_LEDS; i < MAX_LEDS; i++)
+   {
+      leds[1][i] = CRGB::Red;
+      FastLED.show();
+   }
+}
+
+void clearNumber(int pos)
+{
   for(int i = 0; i < NUM_LEDS; i++)
   {
-    leds[i] = CRGB::Black;
+    leds[pos][i] = CRGB::Black;
+    FastLED.show();
   }
+}
+
+void setNumber(int num, int pos){
+  clearNumber(pos);
   
   if(num == 0)
   {
     for(int i = 10; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 1)
   {
     for(int i = 10; i < 20; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 60; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 2)
   {
     for(int i = 0; i < 30; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 40; i < 60; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 3)
   {
     for(int i = 0; i < 30; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 50; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 4)
   {
     for(int i = 0; i < 20; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 30; i < 40; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 60; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 5)
   {
     for(int i = 0; i < 10; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 20; i < 40; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 50; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 6)
   {
     for(int i = 0; i < 10; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 20; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 7)
   {
     for(int i = 10; i < 30; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 60; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else if(num == 8)
   {
     for(int i = 0; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
   else
   {
     for(int i = 0; i < 40; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
     for(int i = 60; i < NUM_LEDS; i = i + 1) {
-      leds[i] = CRGB::Red;
+      leds[pos][i] = CRGB::Red;
       FastLED.show();
     }
   }
---
+++ RecieveTestNumbers/RecieveTestNumbers.ino
@@ -0,0 +1,300 @@
+
+#include <HamShield.h>
+#include <DDS.h>
+#include <packet.h>
+
+#include "FastLED.h"
+
+#define PWM_PIN 3
+#define RESET_PIN A3
+#define SWITCH_PIN 2
+
+#define NUM_LEDS 70
+#define MAX_LEDS 72
+
+#define TENS_PIN 6
+#define ONES_PIN 7
+#define DEC_PIN 8
+
+#define TENS_POS 0
+#define ONES_POS 1
+#define DEC_POS 2
+
+HamShield radio;
+DDS dds;
+AFSK afsk;
+
+String hamPrice = "";
+String price = "";
+String prevPrice = "";
+char* arr;
+
+CRGB leds[3][MAX_LEDS];
+char prevNum[3] = {-1,-1,-1};
+
+void setup() {
+  // NOTE: if not using PWM out, it should be held low to avoid tx noise
+  pinMode(PWM_PIN, OUTPUT);
+  digitalWrite(PWM_PIN, LOW);
+  
+  // prep the switch
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  
+  // set up the reset control pin
+  pinMode(RESET_PIN, OUTPUT);
+  // turn on radio
+  digitalWrite(RESET_PIN, HIGH);
+
+  //Serial3.begin(9600);
+  Serial.begin(9600);
+  
+  delay(100);
+  radio.initialize();
+  radio.frequency(145010);
+  //radio.frequency(152840);
+  radio.setSQOff();
+  delay(100);
+  radio.setModeReceive();
+  delay(100);
+  dds.start();
+  delay(100);
+  afsk.start(&dds);
+  delay(100);
+  dds.setAmplitude(255);
+  
+  FastLED.addLeds<WS2811, TENS_PIN, GRB>(leds[TENS_POS], MAX_LEDS);
+  FastLED.addLeds<WS2811, ONES_PIN, GRB>(leds[ONES_POS], MAX_LEDS);
+  FastLED.addLeds<WS2811, DEC_PIN, GRB>(leds[DEC_POS], MAX_LEDS);
+
+  clearNumber(TENS_POS);
+  clearNumber(ONES_POS);
+  clearNumber(DEC_POS);
+  
+  //setDecPoint();
+}
+
+uint32_t last = 0;
+void loop() {
+  if(afsk.decoder.read() || afsk.rxPacketCount()) {
+      // A true return means something was put onto the packet FIFO
+      // If we actually have data packets in the buffer, process them all now
+      while(afsk.rxPacketCount()) {
+        AFSK::Packet *packet = afsk.getRXPacket();
+        //Serial.print(F("Packet: "));
+        if(packet) {
+          Serial.println("Got Packet");
+          arr = packet->printPacket();
+          String myString = String(arr);
+          if(!myString.equals(hamPrice))
+          {
+            hamPrice = myString;
+            price = stringToPrice(hamPrice);
+          }
+          AFSK::PacketBuffer::freePacket(packet);
+          //Serial.println("The price of Energy is " + price + " cents/kWh");
+        }
+      }
+    }
+
+  if(!prevPrice.equals(price))
+  {
+      for(int i = 0; i < 3; i++)
+      {
+        prevNum[i] = -1;
+        clearNumber(i);
+        clearDecPoint();
+      }
+
+    Serial.println("New Number");
+    setDecPoint();
+    writeNum(price.toDouble());
+    prevPrice = price;
+    delay(500);
+  }
+
+}
+
+//TODO: d2 is the switch input, so remove this
+ISR(ADC_vect) {
+  static uint8_t tcnt = 0;
+  TIFR1 = _BV(ICF1); // Clear the timer flag
+  //PORTD |= _BV(2); // Diagnostic pin (D2)
+  //dds.clockTick();
+  afsk.timer();
+  //PORTD &= ~(_BV(2)); // Pin D2 off again
+}
+
+String stringToPrice(String s){
+  int pos = 0;
+  String toReturn;
+  while(s.substring(pos, pos + 1) != ":"){
+    pos++;  
+  }
+  pos++;
+  toReturn = s.substring(pos, pos + 3);
+  return toReturn;
+}
+
+void writeNum(double value)
+{
+  int val = (int)(value * 10);
+
+  double num[3];
+  num[DEC_POS] = val % 10;
+  val = (val - (val % 10))/10;
+  num[ONES_POS] = val % 10;
+  val = (val - (val % 10))/10;
+  num[TENS_POS] = val % 10;
+
+  for(int i = 0; i < 3; i++)
+  {
+    if(num[i] != prevNum[i])
+    {
+      setNumber(num[i], i);
+      prevNum[i] = num[i];
+    }
+  }
+}
+
+void setDecPoint(void)
+{
+   for(int i = NUM_LEDS; i < MAX_LEDS; i++)
+   {
+      leds[1][i] = CRGB::Red;
+      FastLED.show();
+   }
+}
+
+void clearDecPoint(void)
+{
+  for(int i = NUM_LEDS; i < MAX_LEDS; i++)
+   {
+      leds[1][i] = CRGB::Black;
+      FastLED.show();
+   }
+}
+
+void clearNumber(int pos)
+{
+  for(int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[pos][i] = CRGB::Black;
+    FastLED.show();
+  }
+}
+
+void setNumber(int num, int pos){
+  clearNumber(pos);
+  
+  if(num == 0)
+  {
+    for(int i = 10; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 1)
+  {
+    for(int i = 10; i < 20; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 60; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 2)
+  {
+    for(int i = 0; i < 30; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 40; i < 60; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 3)
+  {
+    for(int i = 0; i < 30; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 50; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 4)
+  {
+    for(int i = 0; i < 20; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 30; i < 40; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 60; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 5)
+  {
+    for(int i = 0; i < 10; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 20; i < 40; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 50; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 6)
+  {
+    for(int i = 0; i < 10; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 20; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 7)
+  {
+    for(int i = 10; i < 30; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 60; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else if(num == 8)
+  {
+    for(int i = 0; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+  else
+  {
+    for(int i = 0; i < 40; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+    for(int i = 60; i < NUM_LEDS; i = i + 1) {
+      leds[pos][i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+}
+
